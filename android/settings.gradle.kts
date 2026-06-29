ce91e19c3064e7669a4cbda6e27c6e35c9f502a2
dossantosnatal-alt <dossantosnatal@gmail.com>
2026-06-29 15:47:41 -0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
+    id("dev.flutter.flutter-gradle-plugin")
     id("com.android.application") version "8.3.2"
     id("kotlin-android") version "1.9.22"
 }
