294aed6c69cdc1bceb512a3c5a4dc9677b9a7bd6
Natal dos Santos <dossantosnatal@gmail.com>
2026-06-30 21:54:28 -0300

    Fix: estrutura pronta para build em nuvem

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,6 @@ allprojects {
     }
 }
 
-<<<<<<< HEAD
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -16,14 +15,6 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-=======
-rootProject.buildDir = layout.buildDirectory.dir("../../build").get().asFile
-
-subprojects {
-    project.layout.buildDirectory.set(rootProject.layout.buildDirectory.dir(project.name))
-}
-
->>>>>>> 5f33b952f513e202af05fcbe9a2199b5687e0803
 subprojects {
     project.evaluationDependsOn(":app")
 }
